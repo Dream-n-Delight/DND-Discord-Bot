64afe242b3142827f2c581e619f6353fe202cf2d
mjudie <arijan.nikoci@gmail.com>
2019-11-20 10:35:42 +0100

    Finished up read() and write()

--- src/org/dnd/dnddiscordbot/tools/TextFileManager.java
+++ src/org/dnd/dnddiscordbot/tools/TextFileManager.java
@@ -1,16 +1,28 @@
 package org.dnd.dnddiscordbot.tools;
 
-import java.io.File;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TextFileManager {
     public TextFileManager(){
     }
 
-    public String[] read(File file){
-        return null;
+    public String[] read(File file) throws IOException {
+        List<String> fileContents = new ArrayList<>();
+        FileReader fr = new FileReader(file);
+        BufferedReader br = new BufferedReader(fr);
+        String line;
+        while((line = br.readLine()) != null){
+            fileContents.add(line);
+        }
+        return fileContents.toArray(new String[0]);
     }
 
-    public boolean write(File file, String[] args){
-        return false;
+    public void write(File file, String[] args) throws IOException{
+        FileWriter writer = new FileWriter(file);
+        for (String line : args){
+            writer.write(line+"\n");
+        }
     }
 }
